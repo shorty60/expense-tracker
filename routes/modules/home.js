42466d4e670b8312e2dc9a57c621f2334fcbb12a
Ting-Yu, Lin <tingyulin68@gmail.com>
2022-11-08 16:04:28 +0800

    feat: add expense receords icon on index

--- routes/modules/home.js
+++ routes/modules/home.js
@@ -1,10 +1,12 @@
 const express = require('express')
 const assert = require('assert')
-const router = express.Router()
-const Record = require('../../models/record')
 const moment = require('moment')
-const { NoRecordsError } = require('../../errors/errortype')
 
+const Record = require('../../models/record')
+const { NoRecordsError } = require('../../errors/errortype')
+const getCategories = require('../../utilities/category') // 取得categories的function
+
+const router = express.Router()
 
 // 瀏覽所有支出 => index page
 router.get('/', async (req, res, next) => {
@@ -16,12 +18,19 @@ router.get('/', async (req, res, next) => {
       records.length,
       new NoRecordsError('目前還沒有支出明細，快來新增第一筆吧')
     )
+    const categories = await getCategories() // 取得種類資料
 
     let totalAmount = 0
     records.forEach(record => {
       totalAmount += record.amount
       record.date = moment(record.date).format('YYYY-MM-DD')
+      categories.forEach(category => {
+        if (record.categoryId.toString() === category._id.toString()) {
+          record['icon'] = category.icon // 新增key-value pairs
+        }
+      })
     })
+
     return res.render('index', { records, totalAmount })
   } catch (err) {
     next(err)
--- utilities/category.js
+++ utilities/category.js
@@ -5,7 +5,7 @@ async function getCategories() {
     //取得icon加入陣列
     const CATEGORY = {
       家居物業: '<i class="fa-solid fa-house"></i>',
-      交通出行: '<i class="fa-solid fa-house"></i>',
+      交通出行: '<i class="fa-solid fa-van-shuttle"></i>',
       休閒娛樂: '<i class="fa-solid fa-face-grin-beam"></i>',
       餐飲食品: '<i class="fa-solid fa-utensils"></i>',
       其他: '<i class="fa-solid fa-pen"></i>',
