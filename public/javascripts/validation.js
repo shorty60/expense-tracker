85cec664c78bfdd1c19f688011167e24ede39c64
Ting-Yu, Lin <tingyulin68@gmail.com>
2022-11-08 17:02:02 +0800

    feat: add expense form front-end validator

--- public/javascripts/validation.js
+++ public/javascripts/validation.js
@@ -1,5 +1,11 @@
-const form = document.querySelector('form')
-const submitButton = document.querySelector('#submit-button')
+let form =
+  document.querySelector('#login-form') ||
+  document.querySelector('#register-form')
+if (!form) {
+  form = document.querySelector('.expense-form')
+}
+// 選擇user登入表單或是編輯支出的表單
+const submitButton = document.querySelector('.submit-button')
 
 submitButton.addEventListener('click', event => {
   form.classList.add('was-validated')
